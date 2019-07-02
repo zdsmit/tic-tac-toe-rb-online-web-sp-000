bcbd73c7e1b99a869d0544e529a01c0931dd1a09
Zach  Smith <zdsmith001@gmail.com>
2019-07-02 22:07:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,11 +31,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    true
-  else
-    false
-  end
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
